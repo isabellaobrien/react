8c89f3d58663c15d08f2d38eaba97b651e648046
isabellaobrien <bellaobrien.it@gmail.com>
2023-11-07 17:16:10 +0000

    functional components

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,7 @@
+
+// function FunctionalGreeting(){
+//     return <h1>Hello from react</h1>
+// }
+const  FunctionalGreeting = () => <h1>Hello from react</h1>;
+
+export default FunctionalGreeting;
